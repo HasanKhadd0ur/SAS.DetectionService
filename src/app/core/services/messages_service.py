d0676162ab9e6d654f27a63a8d1c7912edd7fe6e
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-22 09:29:38 +0300

    Add Prompt Service

--- src/app/core/services/messages_service.py
+++ src/app/core/services/messages_service.py
@@ -16,16 +16,18 @@ class MessageService:
             messages = [
                 Message(
                     id=str(uuid.uuid4()),  # Generate a unique ID
-                    platform=raw_message.get('platform', 'default'),   # Kafka as the source
+                    platform=raw_message.get('platform', 'dummy'), 
+                    platform_id=raw_message.get('platform__id', "1e9cfd36-e1e8-49dc-abe7-d8e983ec2dd3"),   # Kafka as the source
                     domain=raw_message.get('domain', 'default'),  # Default to 'default' if not found
                     source=raw_message.get('source', 'unknown'),  # Default to 'unknown' if not found
                     raw_content=raw_message.get('raw_content', ''),  # Get raw content from Kafka message
                     content=raw_message.get('content', ''),  # Get content from Kafka message
                     sentiment_label=raw_message.get('sentiment_label', 'neutral'),  # Default sentiment label
                     sentiment_score=raw_message.get('sentiment_score', 0.0),  # Default sentiment score
-                    timestamp=datetime.utcnow(),  # Set the timestamp to current UTC time
+                    created_at= datetime.utcnow(),  # Set the timestamp to current UTC time
                     metadata=raw_message.get('metadata', {})  # Get metadata if available, else default to empty dict
                 )
                 for raw_message in raw_messages  # Iterate over the batch of messages
             ]
+            # print(messages[0].created_at)
             yield messages  # Yield the batch of processed messages
